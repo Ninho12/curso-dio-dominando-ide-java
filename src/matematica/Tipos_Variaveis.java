43971eda285bcab08f24529ab543b94019ae7d98
João Paulo Sena Padilha <joaopaulojpsp@gmail.com>
2022-09-06 15:45:33 -0300

    Adição do arquivo Tipos_Variaveis

---
+++ src/matematica/Tipos_Variaveis.java
@@ -0,0 +1,24 @@
+package matematica;
+
+public class Tipos_Variaveis {
+	
+	public static void main(String[] args) {
+		
+		// Variaveis inteiras
+		byte kiloByte, megaByte;
+		short a, b;
+		int i, j;
+		long grande, muitoGrande;
+		
+		// Variaveis tipo de ponto flutuante
+		float bitcoins, etherium;
+		double reais, dollares;
+		
+		// Variaveis tipo de texto
+		char vocal, consoante;
+		String palavra, frase;
+		
+		
+	}
+
+}
