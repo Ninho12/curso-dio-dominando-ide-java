216093fef2e991a082b7c8445da84d3b86cb0666
João Paulo Sena Padilha <joaopaulojpsp@gmail.com>
2022-09-06 15:24:36 -0300

    Adição de um novo arquivo: Variaveis.java

---
+++ src/matematica/Variaveis.java
@@ -0,0 +1,25 @@
+package matematica;
+
+public class Variaveis {
+	
+	public static void main(String[] args) {
+		
+		int i = 12;
+	//  int i = 14;
+		int I = 32;
+	//	int _educacao = 10;
+		
+		int quantidadeCarro = 22;
+	//	int QuantidadeCarro = 21;
+		final String CARRO_DE_MARCA = "Ferrari";
+	//  final String carro_de_marca;
+	//	int quant_com = 12;
+		
+		System.out.println("O i é: "+i);
+		System.out.println("O I grande é: "+I);
+		System.out.println("A quantidade de carro eh: "+quantidadeCarro);
+		System.out.println("A constante éh: "+ CARRO_DE_MARCA);
+		
+	}
+
+}
