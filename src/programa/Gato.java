1e9f55d31bed7bf9f0bc75ed87a50373385d55b4
João Paulo Sena Padilha <joaopaulojpsp@gmail.com>
2022-09-06 09:28:49 -0300

    Primeiro Commit

---
+++ src/programa/Gato.java
@@ -0,0 +1,39 @@
+package programa;
+
+public class Gato {
+	
+	private String nome;
+	private Integer idade;
+	private String cor;
+	
+
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public void falar() {
+		
+		System.out.println("Meu nome eh"+ this.nome);
+		System.out.println("Minha idade eh: "+ this.idade);
+		System.out.println("Minha cor eh: "+ this.cor);
+	}
+
+}
---
+++ src/programa/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package programa;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		
+		Gato meau = new Gato();
+		
+		meau.setCor("Branco");
+		meau.setIdade(5);
+		meau.setNome("Meau");
+		
+		meau.falar();
+	}
+	
+	
+}
