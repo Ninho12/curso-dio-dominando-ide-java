4ad5ea7a2cd75255c6d37436705cd17b865e9a45
João Paulo Sena Padilha <joaopaulojpsp@gmail.com>
2022-09-06 17:02:52 -0300

    Adição do arquivo Conversoes.java

---
+++ src/matematica/Conversoes.java
@@ -0,0 +1,26 @@
+package matematica;
+
+/*
+ * Neste pequeno exercicios eu aprendi a converter em tipo
+ * mais alto para mais baixo e um tipo de valor mais baixo no valor 
+ * mais alto.
+ * 
+ * */
+
+public class Conversoes {
+
+	public static void main(String[] args) {
+		
+	//  insplicito ou seja não precisa colocar o conversor (nome do tipo)
+		int i = 100;
+		long l = 1;
+		
+	//  explicito	ou seja precisa colocar o conversor ( nome do tipo)
+		double d = 20.90000900;
+		float f = (float) d;
+		
+		
+		
+	}
+	
+}
